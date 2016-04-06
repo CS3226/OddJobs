c6f8b88322b53dc09c4cde0c71a03bb5cce24bb8
YongJieYongJie <KhooYongJie@gmx.com>
2016-04-06 15:54:11 +0800

    Adds action mailer

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,13 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+ActionMailer::Base.smtp_settings = {
+  :user_name => 'YongJieYongJie',
+  :password => 'p%0vv"e-jW_y_FfVJ2(=',
+  :domain => 'yourdomain.com',
+  :address => 'smtp.sendgrid.net',
+  :port => 587,
+  :authentication => :plain,
+  :enable_starttls_auto => true
+}
