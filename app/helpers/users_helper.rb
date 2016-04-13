b8d8f80ab480b0632399811fafcc1bf8de21022d
YongJieYongJie <KhooYongJie@gmx.com>
2016-04-13 08:59:47 +0800

    Adds RecordNotFound exception handling for users_helper.rb

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,18 +1,29 @@
 module UsersHelper
-  	def get_application_by_id(id)
-    	JobApplication.find(id)
-  	end	
-	def get_listing_title (listing_id) 
-		Listing.find(listing_id).title
-	end
-	def get_listing_title_by_app(app_id)
-		Listing.find(JobApplication.find(app_id).listing_id).title
-	end	
-	def get_listing (listing_id) 
-		Listing.find(listing_id)
-	end	
+  def get_application_by_id(id)
+    JobApplication.find(id)
+  end	
 
-	def get_user_name (user_id)
-		User.find(user_id).name
-	end
+  def get_listing_title (listing_id) 
+    begin
+      Listing.find(listing_id).title
+    rescue ActiveRecord::RecordNotFound
+      ''
+    end
+  end
+
+  def get_listing_title_by_app(app_id)
+    Listing.find(JobApplication.find(app_id).listing_id).title
+  end	
+
+  def get_listing (listing_id) 
+    begin
+      Listing.find(listing_id)
+    rescue ActiveRecord::RecordNotFound
+      ''
+    end
+  end	
+
+  def get_user_name (user_id)
+    User.find(user_id).name
+  end
 end
